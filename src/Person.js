fe402b8aa704fcc3c6ac571cb49d146fb101fa33
Audrey <keozon@gmail.com>
2023-09-23 21:46:04 +0200

    error handling

--- src/Person.js
+++ src/Person.js
@@ -5,6 +5,10 @@ const Person = ({ person }) => {
     return <p>Loading...</p>;
   }
 
+  if (person.error) {
+    return <p>Error: Unable to fetch data</p>;
+  }
+
   return (
     <ul>
       <li>First name: {person.first_name}</li>
